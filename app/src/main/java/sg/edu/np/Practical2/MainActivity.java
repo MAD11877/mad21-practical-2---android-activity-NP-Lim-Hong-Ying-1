44144b7ff7df0ab6341f7dc504c04e9f0804c8f6
NP Lim Hong Ying <s10223298@connect.np.edu.sg>
2022-04-28 14:53:39 +0800

    Updated layout file

--- app/src/main/java/sg/edu/np/Practical2/MainActivity.java
+++ app/src/main/java/sg/edu/np/Practical2/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +16,19 @@ public class MainActivity extends AppCompatActivity {
 
         User example = new User("John", "Test", 1, false);
 
+        Button followbutton = (Button) findViewById(R.id.button3);
+        boolean followed = example.getFollowed();
+        if (followed == false) {
+            followbutton.setText("Follow");
+        }
+
+        else if (followed == true) {
+            followbutton.setText("Unfollow");
+        }
+
         String name = example.getName();
         String description = example.getDescription();
         int id = example.getId();
-        Button followbutton = (Button) findViewById(R.id.button3);
         followbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,4 +45,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    /*public void onClick(View view) {
+        boolean followed = example.getFollowed();
+        if (followed == false) {
+            example.setFollowed(true);
+            followbutton.setText("Follow");
+        }
+
+        else if (followed == true) {
+            example.setFollowed(false);
+            followbutton.setText("Unfollow");
+        }
+    }*/
 }
